456a09149a23afe1321abc95e3c5cb557fb0b265
James_Calvo <15jcalvo@gmail.com>
2022-10-09 22:41:10 -0400

    first commit- app built

---
+++ src/api/server.js
@@ -0,0 +1,11 @@
+const user1 = {
+   name: 'Name 1',
+   transactions: [
+       {purchase: 'purchase1', 
+        cost: 45.00},
+        {purchase: 'purchase2',
+        cost: 9.45},
+        {purchase: 'purchase3',
+        cost: 110.97}
+    ]
+}
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            <div>
+                <h1>Reward Points Viewer</h1>
+                <h3>Click a user for more details</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Main
